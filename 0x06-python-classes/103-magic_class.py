aa105564dae6918d2e775b65d472e2258eb68b84
James14153 <jamesvincent903@gmail.com>
2023-01-29 22:00:04 -0800

    updated

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-"""Define a  MagicClass matching exactly a bytecode provided by Holberton."""
+"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
 
 import math
 
@@ -10,6 +9,7 @@ class MagicClass:
 
     def __init__(self, radius=0):
         """Initialize a MagicClass.
+
         Arg:
             radius (float or int): The radius of the new MagicClass.
         """
@@ -24,3 +24,4 @@ class MagicClass:
 
     def circumference(self):
         """Return The circumference of the MagicClass."""
+        return (2 * math.pi * self.__radius)
