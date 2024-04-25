6ca3e36e49837a5765d78262c87ff16df9c924dd
james14153 <jamesvincent903@gmail.com>
2024-04-24 21:57:55 -0700

    correcting  class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""creates a square class"""
 
 class Square:
+    """class representing a square
+
+    private attribute:
+    __size(int): takes the size of side of square
+    """
     def __init__(self, size):
+        """initializes a square
+
+        args:
+            size(int): size of a side of the square
+        Returns: None
+        """
         self.__size = size
