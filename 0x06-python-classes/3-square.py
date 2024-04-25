15307ae84385f2c81cf3e44a8284a24708459acc
james14153 <jamesvincent903@gmail.com>
2024-04-24 23:48:36 -0700

    finding the area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """ represents the square
+    Attributes:
+        __size(int): one side of the square
+        """
+
+    def __init__(self, size):
+        """ Initializes the square
+        Args:
+        __size(int):one side of the square
+        Returns:
+        None"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Calculates the area of a square
+        Returns:
+        Area of the square"""
+        return (self.__size) ** 2
