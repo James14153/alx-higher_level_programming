1e3a5eef1ff30447e6500b3c1d576aab5dd12081
james14153 <jamesvincent903@gmail.com>
2024-04-28 22:01:51 -0700

    creates a class rectangle with attributes width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+""" Defines a class rectangle """
+
+
+class Rectangle:
+    """ Represents the rectangle """
+    
+    def __init__(self, width=0, height=0):
+        """ initializes the rectangle
+            args:
+                width (int): smallest side of the rectangle
+                height (int): longest side of the rectangle
+            returns:
+                nothing
+                """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ getter for width
+            returns:
+                width of the rectangle
+                """
+        return self.__width
+
+    @setter.width
+    def width(self, value):
+        """ setter for width
+            args:
+                value (int): the value of width
+            return:
+                nothing
+            """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif  value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ getter for height
+            returns:
+                height of the rectangle
+                """
+        return self.__height
+
+    @setter.height
+    def height(self, value):
+        """ setter for height
+        args:
+            value (int): value of the height
+        returns:
+            nothing
+            """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
