15719df43ac24639c29c99d8661b60133791b1c2
james14153 <jamesvincent903@gmail.com>
2024-06-05 16:24:39 +0300

    checks number of attributes and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+    This function checks for the availaible attributes and methods 
+"""
+
+def lookup(obj):
+    """ Returns list of available methods and object """
+    return dir(obj)
